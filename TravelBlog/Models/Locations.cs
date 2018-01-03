7c57d4fdd1e010dccc5c28f29617b9b9a8e4d96f
Joseph Tomlinson and Kimberly Bordon <joseph+kimberly@>
2018-01-03 10:19:15 -0800

    start in Location Migration and add README

---
+++ TravelBlog/Models/Locations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TravelBlog.Models
+{
+    [Table("Locations")]
+    public class Locations
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ TravelBlog/Models/TravelBlogDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace TravelBlog.Models
+{
+    public class TravelBlogDbContext : DbContext
+    {
+        public TravelBlogDbContext()
+        {
+        }
+
+        public DbSet<Location> Locations { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseMySql(@"Server=localhost;Port=8889;database=TravelBlog;uid=root;pwd=root;");
+        }
+
+        public TravelBlogDbContext(DbContextOptions<TravelBlogDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
